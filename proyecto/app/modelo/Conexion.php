bf9213dcebe7489aad936031720006b1b9d2b773
Bruno Correa <srapecs199@gmail.com>
2026-07-25 02:48:37 -0300

    Fix: ruta correcta para logout

--- proyecto/app/modelo/Conexion.php
+++ proyecto/app/modelo/Conexion.php
@@ -1,15 +1,26 @@
 <?php
+class Conexion {
 
-class Conexion
-{
-    public static function conectar()
-    {
-        $conn = new mysqli("localhost", "root", "", "yoayudo");
+    public static function conectar() {
 
-        if ($conn->connect_error) {
-            die("Error de conexión: " . $conn->connect_error);
+        $host = "localhost";
+        $db = "yoayudo";
+        $user = "root";
+        $pass = "";
+
+        try {
+            $conn = new PDO(
+                "mysql:host=$host;dbname=$db;charset=utf8",
+                $user,
+                $pass
+            );
+
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return $conn;
+
+        } catch (PDOException $e) {
+            die("Error de conexión: " . $e->getMessage());
         }
-
-        return $conn;
     }
 }
